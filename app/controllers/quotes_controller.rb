9e65e1dca6d536d18197cbec8c2c24d5eaa358cf
Your Name <you@example.com>
2019-09-07 19:08:18 +0000

    splurty complete

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,29 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
-end
+
+  
+
+  def create
+    @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
+    redirect_to root_path
+  end
+
+  def about
+  end
+    
+  end
+
+  private
+
+def quote_params
+  params.require(:quote).permit(:saying, :author)
+  end
+
+
+
+
+
